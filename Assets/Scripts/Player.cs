2317bd5ee14783673d579b348c59522aeb942ea6
VicCTS <victordm.cts@gmail.com>
2023-02-17 12:00:18 +0100

    Pooling

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform gunPosition;
+
+    public int ammoType;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = PoolManager.Instance.GetPooledObjects(ammoType, gunPosition.position, gunPosition.rotation);
+            bullet.SetActive(true);
+        }
+    }
+}
